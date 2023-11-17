ad1277249fe36b34a60a512bbf4e5854affd03ca
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-11-17 14:44:35 +0900

    feat: Done fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,18 @@
-for i in range(1,16+1):
 
-	if i%3==0:
-		print('fizz')
-	
-	else:
-		print(i)
+
+
+
+
+
+
+
+for i in range(1, 16 + 1):
+    if i % 15 == 0:
+        print('fizzbuzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
+
